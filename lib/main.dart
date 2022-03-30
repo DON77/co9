f01fb9bca3d4329b6bbc588ee5e3682dcc09ab79
armen.sadoyan <ceo@aod.am>
2022-03-30 16:02:58 +0400

    setting proper url strategy for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'view/co9App.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(const Co9App());
+
 }
 
 
