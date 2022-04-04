94cb91d71b5c510fe7381def4e2af85196e57539
armen.sadoyan <ceo@aod.am>
2022-04-04 11:59:15 +0400

    styling

--- lib/pages/signup_form.dart
+++ lib/pages/signup_form.dart
@@ -14,51 +14,54 @@ class SignUpForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 300,
-      child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                EmailField(
-                  emailController: emailController,
-                ),
-                const Padding(padding: EdgeInsets.all(10.0)),
-                PasswordField(
-                  passwordController: passwordController,
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFFD45130)),
+    return Card(
+      shadowColor: Colors.blue,
+      child: SizedBox(
+        width: 300,
+        child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  EmailField(
+                    emailController: emailController,
                   ),
+                  const Padding(padding: EdgeInsets.all(10.0)),
+                  PasswordField(
+                    passwordController: passwordController,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(const Color(0xff88BDCD)),
+                    ),
 
 
-                  onPressed: () {
-                    var _validated = _formKey.currentState?.validate();
-                    if (_validated == true) {
-                      user.email = emailController?.text;
-                      user.password = passwordController?.text;
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const  SnackBar(content: Text('Processing Data')));
-                      CreateUser instance =
-                          CreateUser(email: user.email.toString(), password: user.password.toString());
-                      instance.saveUser();
-                    }
-                  },
-                  child: const Text(
-                      'Գրանցել Ձեր օրինակը',
-                  style: TextStyle(
-                    color: Colors.white,
-                  )),
-                ),
-              ],
-            ),
-          )),
+                    onPressed: () {
+                      var _validated = _formKey.currentState?.validate();
+                      if (_validated == true) {
+                        user.email = emailController?.text;
+                        user.password = passwordController?.text;
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const  SnackBar(content: Text('Processing Data')));
+                        CreateUser instance =
+                            CreateUser(email: user.email.toString(), password: user.password.toString());
+                        instance.saveUser();
+                      }
+                    },
+                    child: const Text(
+                        'Գրանցել Ձեր օրինակը',
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                  ),
+                ],
+              ),
+            )),
+      ),
     );
   }
 }
